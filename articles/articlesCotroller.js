cd183a032a902e17d6b06b5da8f8b1d1438e8b14
iuri195325 <iurialves0111@gmail.com>
2022-01-07 20:41:59 -0300

    relacionamento entre as tabelas de category y artigos

--- articles/articlesCotroller.js
+++ articles/articlesCotroller.js
@@ -4,8 +4,53 @@ const Category = require('../categorys/category');
 const Article = require('../articles/article');
 const slugify  = require('slugify');
 
+router.get('/home', (req, res) => {
+    Article.findAll({
+        order: [
+            ['id', 'DESC'],
+        ]
+    }).then(articles => {
+        Category.findAll().then(category => {
+            res.render('index', { articles: articles, category: category});
+        });
+    });
+});
+
+router.get('/artigos/:id', (req, res) => {
+    var id = req.params.id;
+    Article.findAll({
+        where: { id: id }
+    }).then(articles => {
+        Category.findAll().then(category => {
+            res.render('artigos', { articles: articles, category: category});
+        });
+    });
+});
+
+router.get('/artigos/category/:slug', (req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: { slug: slug },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+           Category.findAll().then(categorys => {
+               res.render('artigoCategory', { article: category.Aticles, category: categorys });
+           });
+        }else{
+            res.send("deu erro mano kk");
+        }
+    })
+});
+
+
 router.get('/articles', (req, res) =>{
-    res.send("rota deArtigos");
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("articles/index", {articles: articles});
+    })
+    
 });
 
 router.get('/admin/articles/new', (req, res) => {
@@ -29,6 +74,24 @@ router.post('/admin/article/save', (req, res) => {
         })
     
 })
+
+router.post('/admin/article/delete', (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if(!isNaN(id)) {
+            Article.destroy({ 
+                where: { id: id }
+            }).then(() => {
+                res.redirect('/articles');
+            })
+        }else{
+            res.redirect('/articles');
+        }
+    }else{
+        res.redirect('/articles');
+    }
+});
+
 
 
 module.exports = router;
